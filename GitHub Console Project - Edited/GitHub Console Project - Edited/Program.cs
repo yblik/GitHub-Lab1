fd94b43cdee10105af585f24eac383e31cf5ff89
JoshGibsonUOH <119119908+JoshGibsonUOH@users.noreply.github.com>
2023-01-05 16:25:52 +0000

    Added Programs

---
+++ GitHub Console Project - Edited/GitHub Console Project - Edited/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Random rand = new Random();
+
+for (int i = 0; i < 9999; i++)
+{
+    Console.ForegroundColor = (ConsoleColor)rand.Next(0, 16);
+    Console.BackgroundColor = (ConsoleColor)rand.Next(0, 16);
+    Console.WriteLine("Hello, World!");
+    Thread.Sleep(100);
+}
+
