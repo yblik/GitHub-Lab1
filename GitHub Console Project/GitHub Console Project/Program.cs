840ca32c6e007d0ce8ab5ce591f8a417888aec57
Benson4beds <benthomaskilby@gmail.com>
2025-10-19 20:40:39 +0100

    Update Program.cs
    ch ch ch ch changes

--- GitHub Console Project/GitHub Console Project/Program.cs
+++ GitHub Console Project/GitHub Console Project/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("ddd, test!");
 Console.WriteLine("World, Hello!");
 Console.WriteLine(". Hello World!");
 Console.WriteLine("!Dlrow ,Olleh");
